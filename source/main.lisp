768e0138c7ce640f16b3e586df5fbf3ad774c8f7
Christopher Mark Gore <cgore@cgore.com>
2021-03-13 00:34:35 -0600

    CLOG hello world

---
+++ source/main.lisp
@@ -0,0 +1,16 @@
+(defpackage #:limbic-main
+  (:use #:cl #:clog)
+  (:export main))
+(in-package :limbic-main)
+
+(defun on-new-window (body)
+  "Handler for each new web browser window."
+  (let ((hello-element (create-child body "<h1>Hello, world!</h1>")))
+    (set-on-click hello-element
+                  (lambda (object)
+                    (declare (ignore object))
+                    (setf (color hello-element) :green))))
+  (run body))
+
+(defun main ()
+  (initialize #'on-new-window))
