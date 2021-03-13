456570fc042c0f17e3c0b39a0a9b44f4c9c1b24c
Christopher Mark Gore <cgore@cgore.com>
2021-03-13 00:48:20 -0600

    Lispier hello world

--- source/main.lisp
+++ source/main.lisp
@@ -6,7 +6,7 @@
 (defun on-new-window (body)
   "Handler for each new web browser window."
   (setf (title (html-document body)) "limbic.fi")
-  (let ((hello-element (create-child body "<h1>Hello, world!</h1>")))
+  (let ((hello-element (create-section body :h1 :content "Hello, world!")))
     (set-on-click hello-element
                   (lambda (object)
                     (declare (ignore object))
