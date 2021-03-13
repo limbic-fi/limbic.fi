87dfcd23df614dcdce46e6a2fbc3da7551048634
Christopher Mark Gore <cgore@cgore.com>
2021-03-13 00:52:09 -0600

    Renaming the browser main entry file

--- source/browser.lisp
+++ source/browser.lisp
@@ -1,7 +1,7 @@
-(defpackage #:limbic-main
+(defpackage #:limbic-browser
   (:use #:cl #:clog)
   (:export main))
-(in-package :limbic-main)
+(in-package :limbic-browser)
 
 (defun on-new-window (body)
   "Handler for each new web browser window."
