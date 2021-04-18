33a23f1d3704eadffc75d42324f89bc4aef4e1f7
Christopher Mark Gore <cgore@cgore.com>
2021-04-17 20:52:32 -0500

    Don't try to get js/ethererum if it doesn't exist

--- source/javascript/ethereum.lisp
+++ source/javascript/ethereum.lisp
@@ -1,18 +1,25 @@
 (defpackage :limbic/javascript/ethereum
   (:use :common-lisp :clog)
   (:export :eth-request-accounts
+           :ethereum-defined?
            :network-version
            :selected-address))
 (in-package :limbic/javascript/ethereum)
 
+(defun ethereum-defined? (object)
+  (not (null (js-execute object "typeof ethereum"))))
+
 (defun eth-request-accounts (object)
-  (js-execute object "ethereum.request({ method: 'eth_requestAccounts' });"))
+  (when (ethereum-defined? object)
+    (js-execute object "ethereum.request({ method: 'eth_requestAccounts' });")))
 
 (defun network-version (object)
-  (js-execute object "ethereum.networkVersion"))
+  (when (ethereum-defined? object)
+    (js-execute object "ethereum.networkVersion")))
 
 (defun selected-address (object)
-  (let ((selected-address (js-query object "ethereum.selectedAddress")))
-    (if (string= selected-address "null")
-        nil
-        selected-address)))
+  (when (ethereum-defined? object)
+    (let ((selected-address (js-query object "ethereum.selectedAddress")))
+      (if (string= selected-address "null")
+          nil
+          selected-address))))
